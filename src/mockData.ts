46e83052ff42872db370cf0e8fe98071a120a2fe
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-05-27 10:00:46 +0300

    delete mockData

--- src/mockData.ts
+++
@@ -1,7 +0,0 @@
-import { UserT } from './types';
-
-
-export const mockUsers: UserT[] = [
-  { id: 1, email: "user1@gmail.com", password: "" },
-  { id: 2, email: "user2@gmail.com", password: "" }
-];
