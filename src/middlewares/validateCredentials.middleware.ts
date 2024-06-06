09574e6bdbb39a98f64ac4597d2ce19768a3855f
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-06-06 13:26:31 +0300

    remove unnecessary constants usages

--- src/middlewares/validateCredentials.middleware.ts
+++ src/middlewares/validateCredentials.middleware.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express-serve-static-core';
 import {
-  CREATE_USER_ERROR_TYPE,
   EMAIL_NOT_VALID,
   PASSWORD_NOT_VALID
 } from '../constants';
@@ -14,7 +13,6 @@ const validateCredentials = (req: Request, res: Response, next: NextFunction) =>
 
   if (emailError) {
     return res.status(400).json({
-      type: CREATE_USER_ERROR_TYPE,
       massage: {
         email: EMAIL_NOT_VALID
       }
@@ -23,7 +21,6 @@ const validateCredentials = (req: Request, res: Response, next: NextFunction) =>
 
   if (passwordError) {
     return res.status(400).json({
-      type: CREATE_USER_ERROR_TYPE,
       massage: {
         password: PASSWORD_NOT_VALID
       }
