68d842039964bab50ab2d045336668e48225652b
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-06-18 14:41:12 +0300

    make BaseException constructor accept message argument

--- src/models/BaseException.ts
+++ src/models/BaseException.ts
@@ -1,19 +1,22 @@
 class BaseException extends Error {
   name = 'BaseException';
+  message = `This is a base exception message. You should override this in your subclass.`;
   statusCode = 500;
-  stack: string;
+  priorErrorStack?: string;
   details?: string;
 
-  constructor(stack: string, details?: string) {
+  constructor(message:string, priorErrorStack?: string, details?: string) {
     // I need to call super() to ensure the prototype chain is correctly established.
     // This allows instances of my custom error class to be recognized as
     // instances of Error.
     // Also, the the Error class has its own properties, such as message, name, and
     // stack, which need to be initialized
     super();
-    this.message = `This is a base exception message. You should override this in your subclass.`;
-    this.stack = stack;
+    this.message = message;
+    this.priorErrorStack = priorErrorStack;
     this.details = details;
+
+    Error.captureStackTrace(this);
   }
 }
 
